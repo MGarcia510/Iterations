6bf821775529355b0f669f2541223a92b1baea05
MGarcia510 <californiasunset33@gmail.com>
2023-07-13 13:04:29 -0700

    Initial Commit

---
+++ Iterations/IterationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IterationsApp.swift
+//  Iterations
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct IterationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
